ddd45bd3af26d344544e90b72ca2460e9b477dc0
makssssko <grinkomaks25@gmail.com>
2025-02-28 16:50:31 +0300

    fourth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import type { ReactNode } from 'react';
 
 const App = (): ReactNode => {
-  const a = 0
-  const b = 2
-  return (<div>Хуй</div>)
+  const a = 0;
+  const b = 2;
+  const c = 1;
+  return <div>Хуй</div>;
 };
 export default App;
