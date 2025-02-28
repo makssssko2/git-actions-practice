6eb43727d76ac31a98e7220deaa76856ec17942c
makssssko <grinkomaks25@gmail.com>
2025-02-28 16:52:00 +0300

    gg

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import type { ReactNode } from 'react';
 
 const App = (): ReactNode => {
-  const a = 0;
-  const b = 2;
-  const c = 1;
+  const obj = {};
   return <div>Хуй</div>;
 };
 export default App;
