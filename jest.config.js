29b3e5cb3a640700efb177d81f164d7d6fb93a6e
makssssko <grinkomaks25@gmail.com>
2025-03-10 20:46:17 +0300

    Yeah buddy

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ export default {
   setupFilesAfterEnv: ['./src/jestSetup.ts'],
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.app.json' }],
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: './configs/tsconfig.app.json' }],
   },
   testEnvironment: 'jest-environment-jsdom',
 };
