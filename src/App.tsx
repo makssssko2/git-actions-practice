eeea96dad0b904c729ba557b2a9ad7523f515b25
makssssko <grinkomaks25@gmail.com>
2025-03-15 22:24:40 +0300

    DEPLOY TEST

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import Paragraph from './components/Paragraph.tsx';
-
 const App = () => {
-  const obj = {};
-  return <Paragraph text="Я люблю гитхаб" />;
+  // eslint-disable-next-line no-loss-of-precision
+  console.log(11111111111111111111111);
+  return <>Я в шкафу прчусь</>;
 };
 export default App;
